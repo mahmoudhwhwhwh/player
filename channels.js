bee9aaecebd3a9b470904e49ccb2147be32fa7c5
KoraTV-App <159579908+mahmoudhwhwhwh@users.noreply.github.com>
2026-06-07 21:25:16 +0300

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,12 +1,21 @@
 const channels = [
+
 {
-name:"Channel 1",
-logo:"https://picsum.photos/300/200",
-url:"https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+name:"Abu Dhabi Sports 1",
+logo:"https://iili.io/C30jBHX.png",
+url:"https://ev-tcn-dxb-cdn.aws.playco.com/live/eds/Abu_Dhabi_Sports_1_HD/DASH/Abu_Dhabi_Sports_1_HD.mpd"
 },
+
 {
-name:"Channel 2",
-logo:"https://picsum.photos/301/200",
-url:"https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+name:"Channel 325803",
+logo:"https://iili.io/C30jBHX.png",
+url:"http://194.60.93.157/proxy?url=http://toytcl.xyz:8080/live//357643467990765/Ofgo3yz8CH/325803.m3u8"
+},
+
+{
+name:"Channel 418113",
+logo:"https://iili.io/C30jBHX.png",
+url:"http://194.60.93.157/proxy?url=http://toytcl.xyz:8080/live//357643467990765/Ofgo3yz8CH/418113.m3u8"
 }
+
 ];
