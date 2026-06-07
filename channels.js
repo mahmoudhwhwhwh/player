766f49706ede6322aeedbcc43aef13f197719c67
KoraTV-App <159579908+mahmoudhwhwhwh@users.noreply.github.com>
2026-06-07 21:16:31 +0300

    Create channels.js

---
+++ channels.js
@@ -0,0 +1,12 @@
+const channels = [
+{
+name:"Channel 1",
+logo:"https://picsum.photos/300/200",
+url:"https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+},
+{
+name:"Channel 2",
+logo:"https://picsum.photos/301/200",
+url:"https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+}
+];
